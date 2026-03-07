e2df9e6d31f8a94d9205d6d58a1a9d7aa4cf8062
hedes1992 <hezewen1992@qq.com>
2026-03-08 01:03:12 +0800

    对trainer_utils.py中涉及模型加载和存储等函数进行注释

--- trainer/trainer_utils.py
+++ trainer/trainer_utils.py
@@ -24,11 +24,13 @@ def get_model_params(model, config):
     shared_expert = sum(p.numel() for n, p in model.named_parameters() if 'mlp.shared_experts.0.' in n) / 1e6
     base = total - (expert * n_routed) - (shared_expert * n_shared)
     active = base + (expert * n_active) + (shared_expert * n_shared)
+    # 计算模型的总参数和激活参数并输出
     if active < total: Logger(f'Model Params: {total:.2f}M-A{active:.2f}M')
     else: Logger(f'Model Params: {total:.2f}M')
 
 
 def is_main_process():
+    # 非多卡运行或者是多卡运行时的卡0, 代表master进程
     return not dist.is_initialized() or dist.get_rank() == 0
 
 
@@ -42,6 +44,7 @@ def get_lr(current_step, total_steps, lr):
 
 
 def init_distributed_mode():
+    # 初始话多卡分布式训练模式
     if int(os.environ.get("RANK", -1)) == -1:
         return 0  # 非DDP模式
 
@@ -58,15 +61,18 @@ def setup_seed(seed: int):
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
+    # 关闭benchmark设置, 防止torch在benchmark模式下有随机操作
     torch.backends.cudnn.benchmark = False
 
 def lm_checkpoint(lm_config, weight='full_sft', model=None, optimizer=None, epoch=0, step=0, wandb=None, save_dir='../checkpoints', **kwargs):
     os.makedirs(save_dir, exist_ok=True)
     moe_path = '_moe' if lm_config.use_moe else ''
+    # 加载地址 ckp_path: 只跟weight参数,hidden_dim和是否有moe有关
     ckp_path = f'{save_dir}/{weight}_{lm_config.hidden_size}{moe_path}.pth'
     resume_path = f'{save_dir}/{weight}_{lm_config.hidden_size}{moe_path}_resume.pth'
 
     if model is not None:
+        # model存在时就进行模型存储和resume存储(包含优化器状态等)
         raw_model = model.module if isinstance(model, DistributedDataParallel) else model
         raw_model = getattr(raw_model, '_orig_mod', raw_model)
         state_dict = raw_model.state_dict()
@@ -90,6 +96,7 @@ def lm_checkpoint(lm_config, weight='full_sft', model=None, optimizer=None, epoc
             'world_size': dist.get_world_size() if dist.is_initialized() else 1,
             'wandb_id': wandb_id
         }
+        # kwargs有的项可以覆盖resume_data来存储
         for key, value in kwargs.items():
             if value is not None:
                 if hasattr(value, 'state_dict'):
@@ -106,22 +113,28 @@ def lm_checkpoint(lm_config, weight='full_sft', model=None, optimizer=None, epoc
         torch.cuda.empty_cache()
     else:  # 加载模式
         if os.path.exists(resume_path):
+            # 要加载外部作为初始权重, 需要读取resume_path
             ckp_data = torch.load(resume_path, map_location='cpu')
             saved_ws = ckp_data.get('world_size', 1)
             current_ws = dist.get_world_size() if dist.is_initialized() else 1
             if saved_ws != current_ws:
+                # 加载resume时需要考虑卡数变化的影响
                 ckp_data['step'] = ckp_data['step'] * saved_ws // current_ws
                 Logger(f'GPU数量变化({saved_ws}→{current_ws})，step已自动转换为{ckp_data["step"]}')
             return ckp_data
+        # 而不是加载ckp_path
         return None
 
 
 def init_model(lm_config, from_weight='pretrain', tokenizer_path='../model', save_dir='../out', device='cuda'):
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+    # 按照配置来初始化模型
     model = MiniMindForCausalLM(lm_config)
 
     if from_weight!= 'none':
+        # 按照from_weight的地址来替换模型的权重
         moe_suffix = '_moe' if lm_config.use_moe else ''
+        # 命名方式和lm_checkpoint中的ckp_path一致
         weight_path = f'{save_dir}/{from_weight}_{lm_config.hidden_size}{moe_suffix}.pth'
         weights = torch.load(weight_path, map_location=device)
         model.load_state_dict(weights, strict=False)
@@ -144,6 +157,7 @@ class SkipBatchSampler(Sampler):
             batch.append(idx)
             if len(batch) == self.batch_size:
                 if skipped < self.skip_batches:
+                    # 前 self.skip_batches的batch被直接抛弃不进行yield
                     skipped += 1
                     batch = []
                     continue
